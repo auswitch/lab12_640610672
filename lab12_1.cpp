4c0f1cf74520386428cf42340d30930705aedec8
auswitch <sawit_cha@cmu.ac.th>
2021-12-20 13:31:48 +0700

    lab12_1 completed

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -8,7 +8,7 @@ int main(){
     string enter, grade[9] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
     cout << "Press Enter 3 times to reveal your future.";
     for(int z = 0; z < 3; z++)
-        getline(cin, enter);
+        cin.get();
     srand(time(0));
     x = rand()%9;
     cout << "You will get " << grade[x] << " in this 261102.";
